c6017b1b472c5c0eec771ff9f8fc53d4972b3a55
Ayodeji Ayodeji <ayodejix2@gmail.com>
2022-09-13 12:21:33 -0700

    print_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,24 @@
 #include "main.h"
-
+  
 /**
- * main - Prints 10x alphabet in lowercase
+ * print_alphabet_x10 - make alphabet x10 times
+ * 
  * Return: 0
  */
-
 void print_alphabet_x10(void);
 {
-char c;
-	int i = 0;
+    char c;
+    int i = 0;
 
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
+        while (i <= 9)
+        {
+                for (c = 'a'; c <= 'z'; c++)
+                {
+                        _putchar(c);
+                }
 
-		_putchar('\n');
+                _putchar('\n');
 
-		i++;
-	}
+                i++;
+        }
 }
