b906a549ba26e8fbc106b5fabc05a7304e3bc38c
Ayodeji Ayodeji <ayodejix2@gmail.com>
2022-09-13 13:25:17 -0700

    task

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,12 +7,12 @@
   */
 int _abs(int n)
 {
-	if (n < 0)
+	if (n >= 0)
+		return (n);
+
+	else
 	{
-		{
-      return (n * -1);
-    }
-    else
-    {
-      return (n);
-    }
+		n *= -1;
+		return (n);
+	}
+}
