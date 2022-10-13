d82539f00781876cdfc6d03bd5ca994d3f435bee
Ayodeji Ayodeji <ayodejix2@gmail.com>
2022-10-13 12:13:21 -0700

    task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,7 +6,7 @@
   * sum_them_all - sums all arguments
   * @n: number of arguments
   * @...: variable list of arguments
-  * Return: sum of all arguments
+  * Return: 0
   */
 int sum_them_all(const unsigned int n, ...)
 {
