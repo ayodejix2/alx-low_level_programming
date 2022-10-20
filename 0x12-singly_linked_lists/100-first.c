baf99adbabb4d1305992c416b9058b193fa68c51
Ayodeji Ayodeji <ayodejix2@gmail.com>
2022-10-20 14:37:29 -0700

    advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * print_before - prints a string before main function is executed
  * Return: 0
  */
-void print_before(void)
+void b(void) __attribute__ ((constructor));
+void b(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
