f963c9b5dfbb9f8b642e7ddd17958254407e8134
Ayodeji Ayodeji <ayodejix2@gmail.com>
2022-10-20 14:28:43 -0700

    task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,20 @@
-#include "lists.h"
 #include <stdlib.h>
-#include <stddef.h>
+#include <string.h>
+#include "lists.h"
+
 /**
-  * free_list - frees a linked list
-  * @head: the beginning of the list
-  */
+ * free_list - function that frees a list_t list
+ * @head: Const double pointer of structure list_t for beginning
+ * Return: Void
+ */
+
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
-	free_list(head->next);
-	free(head->str);
-	free(head);
+	list_t *temp;
+
+	for (temp = head; temp != NULL; temp = temp->next)
+	{
+		free(temp->str);
+		free(temp);
+	}
 }
