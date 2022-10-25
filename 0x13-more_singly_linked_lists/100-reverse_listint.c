76282ed97159e0f4ac90d1d2ee7ac0d7208f395d
Ayodeji Ayodeji <ayodejix2@gmail.com>
2022-10-25 13:39:53 -0700

    advanced

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,13 +1,28 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * reverse_listint - define function
-  * @head: describe argument
-  * Return: what does it return?
-  */
+ * reverse_listint - return sum of list data
+ * @head: head node
+ * Return: pointer to first node of reversed list
+ */
 
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *prev, *next;
+
+	if (!head || !*head)
+		return (*head);
+
+	prev = 0;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
 }
